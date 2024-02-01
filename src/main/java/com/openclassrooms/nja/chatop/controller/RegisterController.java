9de7710c7d8813b64becbd4f702626746d9b5051
Alyriana <128604938+Alyriana@users.noreply.github.com>
2024-02-01 11:44:28 +0100

    Refactor Login and Register Controllers, update services
    Simplified the authentication in Login and Register Controllers by leveraging UserService. Removed unnecessary exception handling and updated JwtService to directly generate token. Also, ensured dependencies are auto-wired and renamed JWTService to JwtService to follow naming conventions. Server.address property from application.properties was also removed.
    
    Signed-off-by: Alyriana <128604938+Alyriana@users.noreply.github.com>

--- src/main/java/com/openclassrooms/nja/chatop/controller/RegisterController.java
+++ src/main/java/com/openclassrooms/nja/chatop/controller/RegisterController.java
@@ -2,10 +2,13 @@ package com.openclassrooms.nja.chatop.controller;
 
 import com.openclassrooms.nja.chatop.dto.request.RegisterDTO;
 import com.openclassrooms.nja.chatop.dto.response.JwtAuthenticationDTO;
-import com.openclassrooms.nja.chatop.exception.NotFoundException;
-import com.openclassrooms.nja.chatop.service.JWTService;
+import com.openclassrooms.nja.chatop.exception.CreationFailureException;
+import com.openclassrooms.nja.chatop.exception.UserAlreadyExistsException;
+import com.openclassrooms.nja.chatop.service.JwtService;
+import com.openclassrooms.nja.chatop.service.UserService;
 import jakarta.validation.Valid;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,30 +25,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class RegisterController {
 
+    @Autowired
+    private final JwtService jwtService;
+    @Autowired
+    private final UserService userService;
+    @Autowired
     private AuthenticationManager authenticationManager;
-    private JWTService jwtService;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody RegisterDTO registerDTO) {
-        Authentication authentication = null;
+        Authentication authentication;
         try {
+            userService.createUser(registerDTO);
             authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             registerDTO.getEmail(),
                             registerDTO.getPassword()
                     )
             );
-        } catch (NotFoundException e) {
+        } catch (UserAlreadyExistsException e) {
             return new ResponseEntity<>("User already exists", HttpStatus.CONFLICT);
-        } catch (Exception e) {
+        } catch (CreationFailureException e) {
             return new ResponseEntity<>("Registration Failed", HttpStatus.UNAUTHORIZED);
         }
-
         SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String jwt = jwtService.generateToken(authentication);
-        if (jwt != null) {
-            return ResponseEntity.ok(new JwtAuthenticationDTO(jwt));
+        String token = jwtService.generateToken(String.valueOf(authentication));
+        if (token != null) {
+            return ResponseEntity.ok(new JwtAuthenticationDTO(token));
         } else {
             return new ResponseEntity<>("Token Generation Failed", HttpStatus.UNAUTHORIZED);
         }
